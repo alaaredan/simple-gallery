a025d04bbf139dd6619e8e8e2b7775991532840d
Alaa <alaaredan1990@gmail.com>
2017-07-26 13:08:13 +0200

    newdsjfls

---
+++ index.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+  var $body = $('body');
+
+  $.getJSON('data.json', function(array) {
+    // we create a jQuery h1 element for the title
+    var $h1 = $('<h1>Welcome</h1>');
+    var $link = $('<a target="_blank" id="author-link"></a>');
+
+    // we create a jQuery list element for the page
+    var $ul = $('<ul id="pager"></ul>');
+
+    // for each items in the array
+    // for (var index = 0; index < array.length; index++) {}
+    array.forEach(function(item, index) {
+      // we create a jQuery list item
+      var $li = $('<li>' + (index + 1) + '</li>');
+
+      $li.on('click', function() {
+        if (index === 0) {
+          $('.prev').addClass('notshown');
+          $('.next').removeClass('notshown');
+        } else if (index === array.length-1) {
+          $('.next').addClass('notshown');
+          $('.prev').removeClass('notshown');
+        } else {
+          $('.next').removeClass('notshown');
+          $('.prev').removeClass('notshown');
+        }
+        $h1.text(item.title);
+        $body.css('background-image', 'url('+item.src+')');
+        $link
+          .text('Photos by ' + item.author)
+          .attr('href', item.link);
+
+        $('li').removeClass('active');
+        $(this).addClass('active');
+      });
+
+      // that we add to the list ($ul)
+      $ul.append($li);
+
+      if (index === 0) $li.trigger('click');
+    });
+
+    $('.next').on('click',function() {
+      var $liactive = $('li.active');
+      var $linext = $liactive.next();
+      $linext.trigger('click');
+    });
+
+    $('.prev').on('click',function() {
+      var $liactive = $('li.active');
+      var $liprev = $liactive.prev();
+      $liprev.trigger('click');
+    });
+
+    $('body').append($h1, $ul, $link);
+
+  });
+
+});
